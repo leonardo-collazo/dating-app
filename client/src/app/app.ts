9e03869e782dd1d84580c52aa36b9785b2145224
Leonardo Collazo Klenina <leonardo.collazo.klenina@gmail.com>
2026-08-26 16:55:49 -0400

    Added HttpClient integration and implemented member retrieval in App component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   imports: [],
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
   protected title = 'client';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    })
+  }
 }
