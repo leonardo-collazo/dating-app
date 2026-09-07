3e4477808ed4957a449bcfd2316cc72e2d3de60b
Leonardo Collazo Klenina <leonardo.collazo.klenina@gmail.com>
2026-09-07 16:34:28 -0400

    Implement user authentication in navigation component with login/logout functionality

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-nav',
   styleUrl: './nav.css',
   templateUrl: './nav.html',
 })
 export class Nav {
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
+
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: response => {
+        console.log(response);
+        this.creds = {};
+      },
+      error: error => alert(error.message),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
