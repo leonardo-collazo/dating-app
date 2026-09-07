d297718a95c90ca44c58a7978711669488ca5b4c
Leonardo Collazo Klenina <leonardo.collazo.klenina@gmail.com>
2026-09-07 16:34:51 -0400

    Add registration component with HTML, CSS, and TypeScript logic

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,30 @@
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisterCreds } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
+
+@Component({
+  imports: [FormsModule],
+  selector: 'app-register',
+  styleUrl: './register.css',
+  templateUrl: './register.html',
+})
+export class Register {
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
+  protected creds = {} as RegisterCreds;
+
+  register() {
+    this.accountService.register(this.creds).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
