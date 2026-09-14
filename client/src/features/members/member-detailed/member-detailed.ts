84335d1d721804a772645a8fba83a291873872bd
Leonardo Collazo Klenina <leonardo.collazo.klenina@gmail.com>
2026-09-13 20:12:25 -0400

    Add member detailed component with HTML, CSS, and TypeScript implementation

---
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-member-detailed',
+  styleUrl: './member-detailed.css',
+  templateUrl: './member-detailed.html',
+})
+export class MemberDetailed {
+}
