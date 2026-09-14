5ae57c8e22e8af84a31997381f6e9cf73ed105a5
Leonardo Collazo Klenina <leonardo.collazo.klenina@gmail.com>
2026-09-13 20:16:03 -0400

    Enhance app configuration by adding app initializer and view transitions support

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitService } from '../core/services/init-service';
+import { delay, finalize } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(() => {
+      const initService = inject(InitService);
+
+      return initService.init().pipe(
+        delay(500),
+        finalize(() => {
+          document.getElementById('initial-splash')?.remove();
+        })
+      );
+    })
   ]
 };
